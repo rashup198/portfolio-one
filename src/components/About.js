8e994ee07159d5bb9bf6997c196073a671c65cf6
Priyanshu <rashup198@gmail.com>
2023-07-06 17:01:54 +0530

    updated

--- src/components/About.js
+++ src/components/About.js
@@ -5,14 +5,13 @@ const About = () => {
     <div name="about" className='w-full  h-screen bg-[#0a192f] text-gray-300' >
 
         <div className='flex flex-col justify-center items-center w-full h-full'>
-            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+            <div className='max-w-[1300px] w-full grid grid-cols-2 gap-8'>
                 <div className='sm:text-right pb-8 pl-4'>
                     <p className='text-4xl font-bold inline border-b-4 border-pink-600'>About</p>
                 </div>
                 <div>
-
                 </div>
-                <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+                     <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
                     <div className='sm:text-right text-4xl font-bold'>
                         <p>
                            Hi, I'm Priyanshu, nice to meet you. Please take a look around and feel free to contact me if you have any questions. 
