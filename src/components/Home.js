1504b796d06fd14024b979fe4b7dd2495dae48e1
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-07-17 19:47:16 +0530

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi'
+import {Link} from 'react-scroll'
+
 
 const Home = () => {
   return (
@@ -16,9 +18,11 @@ const Home = () => {
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a Frontend developer crafting immersive web experiences, optimizing performance, and collaborating to deliver pixel-perfect products. Let's create exceptional digital experiences together! </p>
 
             <div>
+            <Link to="project" smooth={true} duration={500}>
                 <button className='group text-white border-2 px-6 py-3 flex items-center hover:bg-pink-600 hover:border-pink-600 duration-200'>View Work 
                 <span className='group-hover:rotate-90 duration-200'><HiArrowNarrowRight className='ml-3'></HiArrowNarrowRight></span>
                 </button>
+            </Link>    
             </div>
 
         </div>
