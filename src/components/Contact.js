274ed98cb5da9206d6b135928320e039e45f2ea7
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-07-18 21:22:56 +0530

    updated contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
+    <>
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
       
     <form method="POST" action='https://getform.io/f/7b6b959e-32b6-4f96-a96b-f02e02c666ee' className='max-w-[600px] flex flex-col w-full h-full'>
@@ -23,8 +24,14 @@ const Contact = () => {
         <button className='text-white border-2 hover:bg-pink-600 hover:border-pink-600 px-4 py-2 my-8 mx-auto flex items-center rounded-md'>Send Message</button>
         
     </form>
-
     </div>
+
+    <footer className=''>
+    <h1 className=' pt-[30px] bg-[#0a192f] text-center text-white text-lg pb-[10px]'>Inspired by dank memes and big dreams °◡°</h1>
+    <h1 className='bg-[#0a192f] text-center text-white text-lg'>© Priyanshu Pandey 2023</h1>
+
+    </footer>
+    </>
   )
 }
 
