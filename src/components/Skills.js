23d3383ef9a0956d83bf485a31addb60214be067
Priyanshu Pandey <priyanshukum198@gmail.com>
2023-12-28 02:11:51 +0530

    Add new skills images

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,6 +5,12 @@ import Tailwind from '../assets/tailwind.png'
 import css from '../assets/css.png'
 import GitHub from '../assets/github.png'
 import html from '../assets/html.png'
+import express from "../assets/express.png"
+import node from "../assets/node.png"
+import mongo from "../assets/mongo.png"
+import aws from "../assets/aws.png"
+import postm from "../assets/postman.png"
+
 
 
 
@@ -21,6 +27,37 @@ const Skills = () => {
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 text-center py-8  '>
+
+                <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
+                    <img src={JavaScript} className='w-30 mx-auto'></img>
+                    <p className='my-4'>JavaScript</p>
+                </div>
+
+                <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
+                    <img src={ReactImg} className='w-30 mx-auto'></img>
+                    <p className='my-4'>React.js</p>
+                </div>
+
+                <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
+                    <img src={node} className='w-30 mx-auto'></img>
+                    <p className='my-4'>Node.js</p>
+                </div>
+
+                <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
+                    <img src={mongo} className='w-30 mx-auto'></img>
+                    <p className='my-4'>MongoDb</p>
+                </div>
+
+                <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
+                    <img src={express} className='w-30 mx-auto mt-[70px]'></img>
+                    <p className='my-4'>Express.js</p>
+                </div>
+                
+                <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
+                    <img src={postm} className='w-[100vw] h-[20vh] mt-[50px]'></img>
+                    <p className=' text-center my-4'>Postman</p>
+                </div>
+
                 <div  className='shadow-md shadow-[#040c16] hover:scale-110 duration-200 m-4'>
                     <img src={html} className='w-30 mx-auto'></img>
                     <p className='my-4'>HTML</p>
@@ -36,14 +73,10 @@ const Skills = () => {
                     <p className='my-4'>Tailwind</p>
                 </div>
 
-                <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={JavaScript} className='w-30 mx-auto'></img>
-                    <p className='my-4'>JavaScript</p>
-                </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={ReactImg} className='w-30 mx-auto'></img>
-                    <p className='my-4'>React</p>
+                    <img src={aws} className='w-30 mx-auto'></img>
+                    <p className='my-4'>AWS</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
