92e4e83cfc4849cf0b76b63da38615f9f6144a27
Priyanshu Kumar Pandey <priyanshukum198@gmail.com>
2024-09-13 23:24:51 +0530

    updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -30,63 +30,63 @@ const Skills = () => {
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 text-center py-8  '>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={JavaScript} className='w-30 mx-auto'></img>
+                    <img src={JavaScript} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>JavaScript</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={ReactImg} className='w-30 mx-auto'></img>
+                    <img src={ReactImg} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>React.js</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={node} className='w-30 mx-auto'></img>
+                    <img src={node} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>Node.js</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={mongo} className='w-30 mx-auto'></img>
+                    <img src={mongo} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>MongoDb</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={express} className='w-30 mx-auto mt-[70px]'></img>
+                    <img src={express} alt='js' className='w-30 mx-auto mt-[70px]'></img>
                     <p className='my-4'>Express.js</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={postm} className='w-[100vw] h-[20vh] mt-[50px]'></img>
+                    <img src={postm} alt='js' className='w-[100vw] h-[20vh] mt-[50px]'></img>
                     <p className=' text-center my-4'>Postman</p>
                 </div>
 
                 <div  className='shadow-md shadow-[#040c16] hover:scale-110 duration-200 m-4'>
-                    <img src={html} className='w-30 mx-auto'></img>
+                    <img src={html} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>HTML</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={css} className='w-30 mx-auto'></img>
+                    <img src={css} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>CSS</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={Tailwind} className='w-30 mx-auto'></img>
+                    <img src={Tailwind} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>Tailwind</p>
                 </div>
 
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={aws} className='w-30 mx-auto'></img>
+                    <img src={aws} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>AWS</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={firebase} className='w-30 mx-auto'></img>
+                    <img src={firebase} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>Firebase</p>
                 </div>
 
                 <div  className='shadow-md m-4 shadow-[#040c16] hover:scale-110 duration-200'>
-                    <img src={GitHub} className='w-30 mx-auto'></img>
+                    <img src={GitHub} alt='js' className='w-30 mx-auto'></img>
                     <p className='my-4'>GitHub</p>
                 </div>
 
